f77d9d0411a4e331ba9ff2b606a78ffbaf934c80
Brandon Weaver <a7xncob@gmail.com>
2020-05-15 18:33:55 -0400

    Add header and footer components.

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/Footer.css';
+
+const Footer = () => {
+	return (
+		<footer>
+			<h1>{"</devtorials>"}</h1>
+		</footer>
+	);
+}
+
+export default Footer;
