f0ac78a4d4a9af2e6296369e82f0a928a567d0f3
Brandon Weaver <a7xncob@gmail.com>
2020-05-15 20:01:29 -0400

    Add mock data and build tutorial summaries.

---
+++ src/Data.js
@@ -0,0 +1,11 @@
+const tutorials = [
+	{ language: "1", title: "Example Title 1", content: "Example content 1." },
+	{ language: "2", title: "Example Title 2", content: "Example content 2." },
+	{ language: "3", title: "Example Title 3", content: "Example content 3." },
+	{ language: "4", title: "Example Title 4", content: "Example content 4." },
+	{ language: "5", title: "Example Title 5", content: "Example content 5." },
+];
+
+export default {
+	tutorials
+}
--- src/containers/Tutorials.js
+++ src/containers/Tutorials.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 
 import '../styles/Tutorials.css';
 
+import tutorials from '../Data';
+
 class Tutorials extends Component {
+	state = tutorials;
+
 	render() {
 		return (
 			<div className="Tutorials">
 				<h3>{`<tutorials type="${this.props.type}">`}</h3>
+				{this.state.tutorials.map((tutorial, index) => {
+					return (
+						<div className="TutorialSummary" key={index}>
+							<a href="/">{tutorial.title}</a>
+							<p>{tutorial.language}</p>
+							<p>{tutorial.content}</p>
+						</div>
+					);
+				})}
 				<h3>{"</tutorials>"}</h3>
 			</div>
 		);
