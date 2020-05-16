644fff3c35f8f265fc4ac2cc5a23be9f30dfd86f
Brandon Weaver <a7xncob@gmail.com>
2020-05-15 20:49:30 -0400

    Update tutorials container styling.

--- src/Data.js
+++ src/Data.js
@@ -4,6 +4,11 @@ const tutorials = [
 	{ language: "3", title: "Example Title 3", content: "Example content 3." },
 	{ language: "4", title: "Example Title 4", content: "Example content 4." },
 	{ language: "5", title: "Example Title 5", content: "Example content 5." },
+	{ language: "6", title: "Example Title 6", content: "Example content 6." },
+	{ language: "7", title: "Example Title 7", content: "Example content 7." },
+	{ language: "8", title: "Example Title 8", content: "Example content 8." },
+	{ language: "9", title: "Example Title 9", content: "Example content 9." },
+	{ language: "10", title: "Example Title 10", content: "Example content 10." }
 ];
 
 export default {
