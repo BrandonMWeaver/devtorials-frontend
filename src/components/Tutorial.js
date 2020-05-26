2ea3ce670057ca328e20a73e00b635f3ac3742e8
Brandon Weaver <a7xncob@gmail.com>
2020-05-26 15:04:48 -0400

    Improve tutorial styling.

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -2,12 +2,26 @@ import React from 'react';
 
 import '../styles/Tutorial.css';
 
+const parseContent = content => {
+	const lines = content.split('\n');
+	let parsedLines = [];
+
+	for (let line of lines) {
+		if (line.startsWith("//")) {
+			line = `<span style="color: #CDCDCD;">${line}</span>`;
+		}
+		parsedLines.push(line);
+	}
+
+	return parsedLines.join('\n');
+}
+
 const Tutorial = props => {
 	return (
 		<div className="Tutorial">
 			<h3>{props.tutorial.title}</h3>
 			<h5>{props.tutorial.language}</h5>
-			<div dangerouslySetInnerHTML={{ __html: props.tutorial.content }} />
+			<div dangerouslySetInnerHTML={{ __html: parseContent(props.tutorial.content) }} />
 		</div>
 	);
 }
